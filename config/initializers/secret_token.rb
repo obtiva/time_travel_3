a871bb2d4ebdaebf2619edddf55a06dae6abc638
Noel Rappin <nrappin@obtiva.com>
2011-03-10 16:51:06 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimeTravel3::Application.config.secret_token = '00f79df939e4b34756a1614073be989d9e5969ae40ebf8d1d54ed5224a7703083e09aa7c70bdfbc877a284c654d37ee6b7868065350d03f77682d52dd8ddfcf0'
